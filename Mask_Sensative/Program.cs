829cc3782e5582b8bd5ce5a7af8c96843754fd1a
Steven Zelenak <stevenmzelenak@gmail.com>
2021-01-18 19:57:37 -0600

    Adding Mask_Sensative program

---
+++ Mask_Sensative/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mask_Sensative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a secret message! I won't tell anyone I promise? ;)");
+            string secretMess = Console.ReadLine();
+
+            for(int i = 0; i < secretMess.Length; i++)
+            {
+                if(i >= secretMess.Length - 4)
+                {
+                    Console.Write(secretMess[i]);
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+                
+            }
+        }
+    }
+}
